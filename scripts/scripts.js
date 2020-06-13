be48be855df1cb68491b81b89f03ea0ac5cb78e5
mac-mini <lexx.mg@gmail.com>
2020-06-13 14:49:10 +0300

    first commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,29 @@
 "use strict";
 
-const date = new Date(),
-      userName = prompt('Как тебя зовут?'),
-      userSurname = prompt('Укажите фамилию'),
-      userBirthYear = prompt('Введите ваш год рождения'),
-      currentYear = date.getFullYear(),
-      age = currentYear - userBirthYear;
+let firstNumber,
+    secondNumber;
 
-if (age < 20) {
-  alert(`Привет ${userName} ${userSurname}!`);
-} else if (age >= 20 && age < 40) {
-  alert(`Добрый день ${userName} ${userSurname}!`);
-} else {
-  alert(`Здравствуйте ${userName} ${userSurname}!`);
+firstNumber = prompt('Введите число');
+
+while ( isNaN(+firstNumber) || firstNumber == '' ) {
+  alert('Ошибка, нужно ввести число!');
+  firstNumber = prompt('Введите число');
+}
+
+secondNumber = prompt('Введите второе число');
+
+while ( isNaN(+secondNumber) || secondNumber == '' ) {
+  alert('Ошибка, нужно ввести число!');
+  secondNumber = prompt('Введите число');
+}
+
+firstNumber = Number(firstNumber);   //можно было бы сравнисать строки,
+secondNumber = Number(secondNumber); //но с числани наверно правельние
+
+if (firstNumber > secondNumber) {
+  alert('Первое чило болье второго');
+} else if (firstNumber < secondNumber) {
+  alert('Первое число меньше второго');
+} else if (firstNumber == secondNumber) {
+  alert('Числа равны');
 }
