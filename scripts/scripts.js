d745f7d40e1cf76e644b319bc24af5f60fc00a08
mac-mini <lexx.mg@gmail.com>
2020-06-14 21:38:49 +0300

    ввод данных от проьзовотеля

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,38 @@
 "use strict";
 
-const startYear = 1900,
-      finishYear = 2020;
+const START_YEAR = 1900,
+      FINICH_YEAR = 2020;
 
-for (let i = startYear; i <= finishYear; i++) {
+let userSartYear,
+    userFinishYear;
+
+userSartYear = +prompt('введите год', '');
+
+while (!userSartYear) {
+  alert('Нужно ввсести число');
+  userSartYear = +prompt('введите год', '');
+}
+
+userFinishYear = +prompt('Введите конечный год', '');
+
+while (!userFinishYear) {
+  alert('Нужно ввсести число');
+  userFinishYear = +prompt('Введите конечный год', '');
+}
+
+while (userSartYear > userFinishYear) {
+  userFinishYear = +prompt(`Конечный год ложен быть больше,
+    сейчас ${userSartYear} > ${userFinishYear}, введите конечный год`, '');
+}
+
+alert(`${userSartYear} - ${userFinishYear}`);
+
+for (let i = userSartYear; i <= userFinishYear; i++) {
   if (i % 4 === 0) {
     if (i % 100 !== 0) {
       console.log(i);
     } else if (i % 400 === 0) {
       console.log(i);
-    }  
+    }
   }
 }
