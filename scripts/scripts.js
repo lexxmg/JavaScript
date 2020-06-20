5b17af4ce55e9dd23432eac25e6c6fbeff25c705
mac-book <lexx.mg@gmail.com>
2020-06-20 09:37:03 +0300

    add function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,38 +1,31 @@
 "use strict";
 
-const START_YEAR = 1900,
-      FINICH_YEAR = 2020;
-
 let userSartYear,
     userFinishYear;
 
-userSartYear = +prompt('введите год', '');
-
-while (!userSartYear) {
-  alert('Нужно ввсести число');
-  userSartYear = +prompt('введите год', '');
-}
-
-userFinishYear = +prompt('Введите конечный год', '');
-
-while (!userFinishYear) {
-  alert('Нужно ввсести число');
-  userFinishYear = +prompt('Введите конечный год', '');
-}
+userSartYear = integerNumber( prompt('введите год', '') );
+userFinishYear = integerNumber( prompt('введите год', '') );
 
 while (userSartYear > userFinishYear) {
-  userFinishYear = +prompt(`Конечный год ложен быть больше,
+  userFinishYear = +prompt(`Конечный год дожен быть больше,
     сейчас ${userSartYear} > ${userFinishYear}, введите конечный год`, '');
 }
 
 alert(`${userSartYear} - ${userFinishYear}`);
 
 for (let i = userSartYear; i <= userFinishYear; i++) {
-  if (i % 4 === 0) {
-    if (i % 100 !== 0) {
-      console.log(i);
-    } else if (i % 400 === 0) {
-      console.log(i);
-    }
+  if ( isLeapYear(i) ) console.log(i);
+}
+
+function isLeapYear(y) {
+  return (y % 4 === 0 && y % 100 !== 0 || y % 400 === 0);
+}
+
+function integerNumber(n) {
+  if ( Number(n) ) {
+    return n;
   }
+
+  alert('Ошибка');
+  return integerNumber( prompt('введите год', '') );
 }
