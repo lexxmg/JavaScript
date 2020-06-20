4ed7370d94a40a3df0a1a5a4ad08acea6bd29305
mac-book <lexx.mg@gmail.com>
2020-06-20 08:31:20 +0300

    only function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,22 +3,8 @@
 let firstNumber,
     secondNumber;
 
-firstNumber = prompt('Введите число');
-
-while ( !isNumber(firstNumber) ) {
-  err();
-  firstNumber = prompt('Введите число');
-}
-
-secondNumber = prompt('Введите второе число');
-
-while ( !isNumber(secondNumber) ) {
-  err();
-  secondNumber = prompt('Введите число');
-}
-
-firstNumber = Number(firstNumber);
-secondNumber = Number(secondNumber);
+firstNumber = checkNumber( prompt('Введите число', '') );
+secondNumber = checkNumber( prompt('Введите число', '') );
 
 if (firstNumber > secondNumber) {
   alert('Первое чило болье второго');
@@ -28,13 +14,17 @@ if (firstNumber > secondNumber) {
   alert('Числа равны');
 }
 
+function checkNumber(n) {
+  if ( isNumber(n) ) {
+    return +n;
+  }
+  alert('Ошибка, нужно ввести число!');
+  return checkNumber( prompt('Введите число', '') );
+}
+
 function isNumber(n) {
   if ( !isNaN(parseFloat(n)) && isFinite(n) ) {
     return true;
   }
   return false;
-}
-
-function err() {
-  alert('Ошибка, нужно ввести число!');
 }
