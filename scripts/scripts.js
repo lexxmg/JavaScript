17653ef65788039455a9b77cc0a109333c0a4ff7
mac-mini <lexx.mg@gmail.com>
2020-07-05 15:59:26 +0300

    first commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,18 +5,32 @@ let userNumber,
 
 randomNumber = getRandomInt(100);
 
+const counter = makeCounter(3);
+
 userNumber = checkNumber( prompt('Попробуй угадать число', '') );
 
 compatisonNumber(randomNumber, userNumber);
 
 
+function makeCounter(count) {
+  return () => count--;
+}
+
 function compatisonNumber(r, n) {
+  const i = counter();
+
+  if (i < 1) {
+    alert('Колличество попыток, закончилось');
+    window.counter = null;
+    return;
+  }
+
   if (n === null) {
     return;
   } else if (r < n) {
-    return compatisonNumber(r, checkNumber( prompt('Меньше!', '') ) );
+    return compatisonNumber(r, checkNumber( prompt('Меньше! ' + i, '') ) );
   } else if (r > n) {
-    return compatisonNumber(r, checkNumber( prompt('Больше!', '') ) );
+    return compatisonNumber(r, checkNumber( prompt('Больше! ' + i , '') ) );
   }
   return alert('Правильно!');
 }
