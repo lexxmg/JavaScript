8eabfb0316aa10c5051b2adbf007508af31dd8a0
mac work <lexxmg@iMac-lexx.localdomain>
2020-07-13 14:19:14 +0300

    sync

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,59 @@
 "use strict";
 
-const users = [
-               {name: 'Иван', login: 'ivan', pass: '213re43'},
-               {name: 'Александр', login: 'alex', pass: '36jh8'},
-               {name: 'Виктория', login: 'vika', pass: '4kh808'}
-              ];
+let users = new Users( prompt('Введите полное имя', '') );
+const us = new UserList;
+//console.log( us.add(users) );
+//console.log( users.str( prompt('Введите полное имя', '') ) );
+console.log(users);
 
-const obj = users.find(function(obj) {
-              for (let param in obj) {
-                if (this === obj[param]) return true;
-              }
-            }, prompt('Ведите имя', ''));
+function UserList() {
+  const arr = [];
 
-if (obj) {
-  const pass = prompt('Введите пароль', '');
-  if (pass === obj.pass) {
-    alert('Здравствуйте, ' + obj.name);
-  } else {
-    alert('Неверный пароль!');
+  this.add = function(obj){
+    arr.push(obj);
+    console.log(users);
+    return users = new Users;
   }
-} else {
-  alert('Имя не найдено!');
 }
+
+function Users(str) {
+  const arr = arr.split(str);
+  const date = new Date();
+
+  this.firstName = arr[0];
+  this.lastName = arr[1];
+  this.regDate = date;
+}
+
+
+
+//console.log(users);
+
+// const users = [
+//                {name: 'Иван', login: 'ivan', pass: '213re43'},
+//                {name: 'Александр', login: 'alex', pass: '36jh8'},
+//                {name: 'Виктория', login: 'vika', pass: '4kh808'}
+//               ];
+//
+// const obj = users.find( find( prompt('Ведите имя', '') ) );
+//
+// function find(name) {
+//   return function(obj) {
+//     for (let key in obj) {
+//       if (name === obj[key]) {
+//         return true;
+//       }
+//     }
+//   }
+// }
+//
+// if (obj) {
+//   const pass = prompt('Введите пароль', '');
+//   if (pass === obj.pass) {
+//     alert('Здравствуйте, ' + obj.name);
+//   } else {
+//     alert('Неверный пароль!');
+//   }
+// } else {
+//   alert('Имя не найдено!');
+// }
