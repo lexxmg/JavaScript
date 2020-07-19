6bfb748b9beee7769df25cb301c7b677f32a1586
mac-book <lexx.mg@gmail.com>
2020-07-19 14:38:56 +0300

    first commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,18 @@
 "use strict";
 
-function User(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
 
-  //this.getFullName = function() {
-  //  return this.firstName + ' ' + this.lastName;
-  //}
+function Vehicles(color) {
+  this.color = color;
 }
 
-User.prototype.getFullName = function() {
-  return this.firstName + ' ' + this.lastName;
+function Car(wheels) {
+  this.wheels = wheels;
 }
 
-function Manager(firstName, lastName) {
-  User.apply(this, arguments);
-
-  this.seyHello = function() {
-    alert('Здравствуйте');
-  }
-
-  this.changeName = function(name) {
-    this.firstName = name;
-  }
-
-  this.getThis = function() {
-    console.log(this);
-  }
+function Airplane(wingspan) {
+  this.wingspan = wingspan;
 }
 
-const u = new User('John', 'Doe');
-const m = new Manager('Jene', 'Doe');
-const m1 = new Manager('иван', 'иванов');
+function Ship(displacement) {
+  this.displacement = displacement;
+}
