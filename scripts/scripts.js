bb19a5a8ad54cb2297756e8b15b57aadd8b607b3
mac-book <lexx.mg@gmail.com>
2020-07-23 22:05:14 +0300

    наследование методов

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,12 @@
 "use strict";
 
-const car = new Car(4, 'white');
 
 
 function Vehicles(color) {
+  this.color = color;
+}
+
+Vehicles.prototype.setColor = function(color) {
   this.color = color;
 }
 
@@ -13,6 +16,10 @@ function Car(wheels, color) {
   this.wheels = wheels;
 }
 
+Car.prototype = Object.create(Vehicles.prototype);
+Car.prototype.constructor = Vehicles;
+
+
 function Airplane(wingspan) {
   this.wingspan = wingspan;
 }
@@ -20,3 +27,42 @@ function Airplane(wingspan) {
 function Ship(displacement) {
   this.displacement = displacement;
 }
+
+const car = new Car(4, 'white');
+const v = new Vehicles('black');
+
+
+let Character = function(settings) {
+  this.name = settings.name;
+  this.health = settings.health || 100;
+  this.exp = settings.exp || 0;
+  this.strength = settings.strength || 1;
+};
+
+Character.prototype.walk = function(steps) {
+  console.log(this.name + ' прошел ' + steps + ' шагов');
+};
+
+Character.prototype.run = function(steps) {
+  console.log(this.name + ' пробежал ' + 2 * steps + ' шагов');
+};
+
+
+let Human = function(settings) {
+  Character.apply(this, arguments); // наследуем все свойства от Character
+};
+
+Human.prototype = Object.create(Character.prototype);  // наследуем методы через прототип
+Human.prototype.constructor = Character;  // прописываем имя конструктора
+
+Human.prototype.build = function(buildingStrength) {
+  this.health += buildingStrength;
+};
+
+let human = new Human({name: 'John', health: 10});
+
+console.log(human.health);
+human.build(10);
+console.log(human.health);
+human.walk(10);
+human.run(50);
