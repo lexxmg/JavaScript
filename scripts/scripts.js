630cc91104926d05124fedd021e61095614e2b4d
mac-mini <lexx.mg@gmail.com>
2020-07-27 21:03:53 +0300

    Proxy

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,33 @@
 "use strict";
 
-const date = new Date(),
-      userName = prompt('Как тебя зовут?'),
-      userSurname = prompt('Укажите фамилию'),
-      userBirthYear = prompt('Введите ваш год рождения'),
-      currentYear = date.getFullYear(),
-      age = currentYear - userBirthYear;
-
-if (age < 20) {
-  alert(`Привет ${userName} ${userSurname}!`);
-} else if (age >= 20 && age < 40) {
-  alert(`Добрый день ${userName} ${userSurname}!`);
-} else {
-  alert(`Здравствуйте ${userName} ${userSurname}!`);
+let obj = {
+  name: 'lexx',
+  lastName: 'mg'
 }
+
+obj = new Proxy(obj, {
+  get(target, prop) {
+    if (prop in target) {
+      return  target[prop];
+    } else {
+      return alert('Нет такого свойчтва');
+    }
+  }
+});
+
+
+
+// const date = new Date(),
+//       userName = prompt('Как тебя зовут?'),
+//       userSurname = prompt('Укажите фамилию'),
+//       userBirthYear = prompt('Введите ваш год рождения'),
+//       currentYear = date.getFullYear(),
+//       age = currentYear - userBirthYear;
+//
+// if (age < 20) {
+//   alert(`Привет ${userName} ${userSurname}!`);
+// } else if (age >= 20 && age < 40) {
+//   alert(`Добрый день ${userName} ${userSurname}!`);
+// } else {
+//   alert(`Здравствуйте ${userName} ${userSurname}!`);
+// }
