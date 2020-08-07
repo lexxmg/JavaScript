e6e7aaf9c00f0c2650833d524c1839e0779926be
mac-book <lexx.mg@gmail.com>
2020-08-07 21:04:35 +0300

    sync

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,16 @@
 "use strict";
 
+const selector = document.querySelector('.container h1');
+const texts = document.querySelectorAll('.container p');
+
+console.log(selector);
+console.log(texts);
+
+for (let i = 0; i < texts.length; i++) {
+  console.log(texts[i].textContent);
+  texts[i].innerHTML = '<span>Новый текст ' + (i + 1) + '</span>';
+}
+
 // const date = new Date(),
 //       userName = prompt('Как тебя зовут?'),
 //       userSurname = prompt('Укажите фамилию'),
