d6b96f503d9f54352a77508dfce3019d6c7112a8
mac-book <lexx.mg@gmail.com>
2020-08-08 21:13:38 +0300

    elements

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,13 +2,38 @@
 
 const selector = document.querySelector('.container h1');
 const texts = document.querySelectorAll('.container p');
+const el = document.querySelector('h1');
+const btn = document.querySelector('button');
+const content = document.querySelector('.container');
+
+const p1 = document.createElement('p');
+p1.innerHTML = '<i>текст в новом поараграфе</i>';
+p1.style.textAlign = 'center';
+content.appendChild(p1);
+console.log(content.children[2] + ' 2 дочерний элемент');
+content.insertBefore(p1, content.children[2]);
+
+
+console.log(el.textContent + ' текст заголовка');
+el.textContent = 'Новый текст';
+console.log( el.getAttribute('class') + ' класс h1');
+
+el.classList.add('hide');
+el.classList.remove('hide');
+
+el.style.color = 'red';
+
+//setInterval( () => el.classList.toggle('hide'), 1000 );
 
 console.log(selector);
 console.log(texts);
 
+btn.setAttribute('disabled', true);
+
+
 for (let i = 0; i < texts.length; i++) {
-  console.log(texts[i].textContent);
-  texts[i].innerHTML = '<span>Новый текст ' + (i + 1) + '</span>';
+  //console.log(texts[i].textContent);
+  //texts[i].innerHTML = '<span>Новый текст ' + (i + 1) + '</span>';
 }
 
 // const date = new Date(),
