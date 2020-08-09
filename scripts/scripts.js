afcea11837b826641683016729fc5cab5ef98e15
mac-book <lexx.mg@gmail.com>
2020-08-09 12:45:13 +0300

    first commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,1 @@
 "use strict";
-
-const date = new Date(),
-      userName = prompt('Как тебя зовут?'),
-      userSurname = prompt('Укажите фамилию'),
-      userBirthYear = prompt('Введите ваш год рождения'),
-      currentYear = date.getFullYear(),
-      age = currentYear - userBirthYear;
-
-if (age < 20) {
-  alert(`Привет ${userName} ${userSurname}!`);
-} else if (age >= 20 && age < 40) {
-  alert(`Добрый день ${userName} ${userSurname}!`);
-} else {
-  alert(`Здравствуйте ${userName} ${userSurname}!`);
-}
