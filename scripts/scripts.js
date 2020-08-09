64dc16426d59dd4486909b7abe05cb723d93aa5f
mac-book <lexx.mg@gmail.com>
2020-08-09 13:17:11 +0300

    onClick

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const btn1 = document.querySelector('.inner-1__btn1');
+const btn2 = document.querySelector('.inner-1__btn2');
+
+btn1.addEventListener('click', () => alert('click'));
+btn2.addEventListener('click', onClick);
+
+function onClick() {
+  alert('onClick');
+  btn2.style.backgroundColor = 'red';
+  btn2.removeEventListener('click', onClick);
+}
