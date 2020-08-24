d745d324b3ea13939717360e21d14ebed5c7f5d9
mac-mini <lexx.mg@gmail.com>
2020-08-24 22:08:23 +0300

    submit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,40 +2,23 @@
 
 const container = document.body.querySelector('.container'),
       list = container.querySelector('.container__list'),
-      inputText = container.querySelector('.container__iput'),
-      btn = container.querySelector('.container__btn');
+      form = container.querySelector('.container__form'),
+      inputText = container.querySelector('.form__iput'),
+      btn = container.querySelector('.form__btn');
 
-container.addEventListener('click', (event) => {
-  if (event.target === btn) {
-    if (inputText.value === '') {
-      alert('Поле ввода должно быть заполнено!');
-    } else {
-      list.insertAdjacentHTML('beforeend', `<li class = "list__item">${inputText.value}</li>`);
-      // const item = document.createElement('li');
-      // list.append(item);
-      // item.className = 'list__item';
-      // item.textContent = inputText.value;
-      inputText.value = '';
-    }
-  }
-
+list.addEventListener('click', (event) => {
   if (event.target.tagName === 'LI') {
     event.target.classList.toggle('list__item--done');
   }
 });
 
-inputText.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    btn.click();
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (inputText.value === '') {
+    alert('Поле ввода должно быть заполнено!');
+  } else {
+    list.insertAdjacentHTML('beforeend', `<li class="list__item">${inputText.value}</li>`);
+    inputText.value = '';
   }
+  console.log(event);
 });
-
-function makeElement(el, text, cl) {
-  el = document.createElement(el);
-  el.innerHTML = text;
-
-  if (cl) {
-    el.className = cl;
-  }
-  return el;
-}
