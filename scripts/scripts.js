cc93c22f2fe86d34d7dcf773eacc6e1b9c977f62
mac-book <lexx.mg@gmail.com>
2020-09-13 09:04:32 +0300

    finish

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,38 +4,36 @@ const textPade = document.querySelector('.container__text'),
       btnEdit = document.querySelector('.container-btn__edit'),
       btnSave = document.querySelector('.container-btn__save'),
       btnCansel = document.querySelector('.container-btn__cansel'),
-      select = document.querySelector('.form__select');
+      btnDelete = document.querySelector('.container-btn__delete'),
+      select = document.querySelector('.container__select');
 
-for (let i = 0; i < localStorage.length; i++) {
-  let elOption = document.createElement('option');
-  elOption.innerHTML = localStorage.key(i);
-  select.append(elOption);
+let textStr;
+
+if (localStorage.length > 0) {
+  select.removeAttribute('disabled');
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let elOption = document.createElement('option');
+    elOption.innerHTML = localStorage.key(i);
+    select.append(elOption);
+  }
+
+  textPade.innerHTML = localStorage.getItem(select.value);
 }
 
 select.addEventListener('change', () => {
-  console.log(select.value);
   textPade.innerHTML = localStorage.getItem(select.value);
-  textStr = localStorage.getItem(select.value);
 });
 
-let textStr;
-let lastName = new Date();
-
-if (localStorage.length > 0) {
-  lastName = localStorage.key(localStorage.length - 1);
-
-  textPade.innerHTML = localStorage.getItem(lastName);
-  textStr = localStorage.getItem(lastName);
-} else {
-  textStr = textPade.innerHTML;
-  localStorage.setItem(lastName, textStr);
-}
-
 btnEdit.addEventListener('click', () => {
+  textStr = localStorage.getItem(select.value);
+  select.setAttribute('disabled', 'disabled');
+
   textPade.setAttribute('contenteditable', 'true');
   btnEdit.setAttribute('disabled', 'disabled');
   btnSave.removeAttribute('disabled');
   btnCansel.removeAttribute('disabled');
+  btnDelete.removeAttribute('disabled');
 });
 
 btnSave.addEventListener('click', () => {
@@ -44,18 +42,35 @@ btnSave.addEventListener('click', () => {
 
   btnSave.setAttribute('disabled', 'disabled');
   btnCansel.setAttribute('disabled', 'disabled');
+  btnDelete.setAttribute('disabled', 'disabled');
   btnEdit.removeAttribute('disabled');
   textPade.setAttribute('contenteditable', 'false');
 
   localStorage.setItem(date, textSave);
+
+  select.innerHTML = '';
 
   for (let i = 0; i < localStorage.length; i++) {
     let elOption = document.createElement('option');
     elOption.innerHTML = localStorage.key(i);
     select.append(elOption);
   }
+
+  select.removeAttribute('disabled');
+  select.value = date;
 });
 
 btnCansel.addEventListener('click', () => {
   textPade.innerHTML = textStr;
+  select.removeAttribute('disabled');
+
+  btnSave.setAttribute('disabled', 'disabled');
+  btnCansel.setAttribute('disabled', 'disabled');
+  btnDelete.setAttribute('disabled', 'disabled');
+  btnEdit.removeAttribute('disabled');
+  textPade.setAttribute('contenteditable', 'false');
+});
+
+btnDelete.addEventListener('click', () => {
+
 });
