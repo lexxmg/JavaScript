d476a8e45f88454fc319f5f34d776a60e804679b
mac-book <lexx.mg@gmail.com>
2020-09-20 21:51:17 +0300

    fetch

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const text = document.querySelector('.main__text'),
-      btn = document.querySelector('.main__btn');
+      btn = document.querySelector('.main__btn'),
+      btnFetch = document.querySelector('.btn-fetch-js');
 
 btn.addEventListener('click', () => {
   const request = new XMLHttpRequest();
@@ -13,3 +14,12 @@ btn.addEventListener('click', () => {
   request.open('GET', '/data.txt');
   request.send();
 });
+
+btnFetch.onclick = () => {
+  fetch('/data.txt').then(res => res.text())
+    .then(data => console.log(data))
+    .catch(err => {
+      console.log('err');
+      console.log(err.text());
+    });
+}
