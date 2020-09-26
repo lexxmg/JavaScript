ca2ab399a88f95156987ce6968935badbf84fd2a
mac-book <lexx.mg@gmail.com>
2020-09-26 21:14:16 +0300

    html css

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,8 @@
 'use strict';
 
-const text = document.querySelector('.main__text'),
-      btn = document.querySelector('.main__btn'),
-      btnFetch = document.querySelector('.btn-fetch-js');
-
-btn.addEventListener('click', () => {
-  const request = new XMLHttpRequest();
-
-  request.addEventListener('load', () => {
-    text.innerHTML = request.responseText;
-  });
-
-  request.open('GET', '/data.txt');
-  request.send();
-});
-
-btnFetch.onclick = async () => {
-  const res = await fetch('/data.txt');
-
-  if (res.ok) {
-    const text = await res.text();
-    console.log(text);
-  } else {
-    console.log(res.status);
-  }
-}
-
-function xhr(url) {
-  return new Promise( (resolve, reject) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('load', () => {
-      if (request.status >= 400) {
-        reject(request.status);
-      } else {
-        resolve(request.responseText);
-      }  
-    });
-
-    request.open('GET', url);
-    request.send();
-  });
-}
-
-const sleep = function() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(() => {
-      resolve();
-    }, 2000);
-  });
-}
+const form = document.querySelector('.search__form'),
+      persName = document.querySelector('.js-name'),
+      persHeight = document.querySelector('.js-height'),
+      persMass = document.querySelector('.js-mass'),
+      persBirthYear = document.querySelector('.js-birth_year'),
+      persFilmCount = document.querySelector('.js-film_count');
