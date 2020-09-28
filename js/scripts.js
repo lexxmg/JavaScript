6412f67d623d24e6ee637b07f256827738bdd3e8
mac-book <lexx.mg@gmail.com>
2020-09-28 20:15:06 +0300

    add function persData

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
 'use strict';
 
-const form = document.querySelector('.search__form'),
-      persName = document.querySelector('.js-name'),
-      persHeight = document.querySelector('.js-height'),
-      persMass = document.querySelector('.js-mass'),
-      persBirthYear = document.querySelector('.js-birth_year'),
-      persFilmCount = document.querySelector('.js-film_count');
+const form = document.querySelector('.search__form');
 
 const api = 'https://swapi.dev/api/';
 
@@ -23,7 +18,7 @@ form.addEventListener('submit', (event) => {
     })
     .then( (result) => {
       if (result.count > 0) {
-        console.log(result.results[0].mass);
+        persData(result.results[0]);
       } else {
         popup('По вашему запросу ничего не райдено.');
       }
@@ -34,9 +29,24 @@ form.addEventListener('submit', (event) => {
 });
 
 
+function persData(obj) {
+  const persName = document.querySelector('.js-name'),
+        persHeight = document.querySelector('.js-height'),
+        persMass = document.querySelector('.js-mass'),
+        persBirthYear = document.querySelector('.js-birth_year'),
+        persFilmCount = document.querySelector('.js-film_count');
+
+  persName.innerHTML = obj.name || 'Параметр неизвестен';
+  persHeight.innerHTML = obj.height || 'Параметр неизвестен';
+  persMass.innerHTML = obj.mass || 'Параметр неизвестен';
+  persBirthYear.innerHTML = obj.birth_year || 'Параметр неизвестен';
+  persFilmCount.innerHTML = obj.films.length || 'Параметр неизвестен';
+}
+
 function popup(text) {
   const popup = document.querySelector('.popup-container');
   const popupText = document.querySelector('.popup-container__text');
+  document.body.classList = 'off-scroll';
 
   popupText.innerHTML = text;
   popup.classList.remove('hidden');
@@ -44,10 +54,12 @@ function popup(text) {
   popup.addEventListener('click', event => {
     if (!event.target.classList.contains('popup-container__popup')) {
       popup.classList.add('hidden');
+      document.body.classList = '';
     }
   });
 
   setTimeout(() => {
     popup.classList.add('hidden');
+    document.body.classList = '';
   }, 5000);
 }
