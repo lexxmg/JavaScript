fe9771735cd9928ac279f893cf85798a6bd586c5
mac-book <lexx.mg@gmail.com>
2020-10-05 20:58:52 +0300

    goal

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 $(function() {
   const field = $('.field'),
         ball = $('.ball'),
-        goalLeft = $('.goalLeft'),
-        goalRight = $('.goalRight');
+        goalLeft = $('.goal-left'),
+        goalRight = $('.goal-right');
 
   ball.on('click', event => {
     if (ball.position().left === 0) {
@@ -12,14 +12,18 @@ $(function() {
         top: getRandomIntInclusive(0, field.outerHeight() - ball.outerHeight()),
         left: field.outerWidth()- ball.outerWidth()
       }, 1000, () => {
-        console.log(ball.position().top);
+        if (ball.position().top >= goalLeft.position().top && ball.position().top - ball.outerHeight() <= goalLeft.position().top) {
+          console.log('гол');
+        }
       });
     } else {
       ball.animate({
         top: getRandomIntInclusive(0, field.outerHeight() - ball.outerHeight()),
         left: 0
       }, 1000, () => {
-        console.log(ball.position().top);
+        if (ball.position().top >= goalLeft.position().top && ball.position().top - ball.outerHeight() <= goalLeft.position().top) {
+          console.log('гол');
+        }
       });
     }
 
