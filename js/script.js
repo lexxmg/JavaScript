40aa9154b28ef06db7f2108bfa485eb69e343d38
mac-book <lexx.mg@gmail.com>
2020-10-05 20:35:47 +0300

    sync

--- js/script.js
+++ js/script.js
@@ -2,53 +2,33 @@
 
 $(function() {
   const field = $('.field'),
-        ball = $('.ball');
-
-  const pos = ballPosition(0, 0, field, ball);
-  ball.css({
-    'display': 'block',
-    'top': pos.y,
-    'left': pos.x
-  });
+        ball = $('.ball'),
+        goalLeft = $('.goalLeft'),
+        goalRight = $('.goalRight');
 
   ball.on('click', event => {
-    const ballPosInnerX = (event.pageX - ball.offset().left) - ball.outerWidth() / 2;
-    const ballPosInnerY = (event.pageY - ball.offset().top) - ball.outerHeight() / 2;
-    const cootrds = ballPosition(-50 * ballPosInnerX, -50 * ballPosInnerY, field, ball);
+    if (ball.position().left === 0) {
+      ball.animate({
+        top: getRandomIntInclusive(0, field.outerHeight() - ball.outerHeight()),
+        left: field.outerWidth()- ball.outerWidth()
+      }, 1000, () => {
+        console.log(ball.position().top);
+      });
+    } else {
+      ball.animate({
+        top: getRandomIntInclusive(0, field.outerHeight() - ball.outerHeight()),
+        left: 0
+      }, 1000, () => {
+        console.log(ball.position().top);
+      });
+    }
 
-    ball.animate({top: cootrds.y, left: cootrds.x}, 1000);
   });
-
-
-  function ballPosition(x, y, field, ball) {
-    const fieldCentrY = field.offset().top + field.outerHeight() / 2;
-    const fieldCentrX = field.offset().left + field.outerWidth() / 2;
-
-    if ( x > fieldCentrX ) {
-      x = (fieldCentrX - ball.outerWidth() / 2) - field.offset().left;
-    }
-
-    if ( x < -fieldCentrX ) {
-      x = -(fieldCentrX - ball.outerWidth() / 2) + field.offset().left;
-    }
-
-    if ( y > fieldCentrY ) {
-      y = (fieldCentrY - ball.outerHeight() / 2) - field.offset().top;
-    }
-
-    if ( y < -fieldCentrY ) {
-      y = -(fieldCentrY - ball.outerHeight() / 2) + field.offset().top;
-    }
-
-    // ball.css({
-    //   'display': 'blick',
-    //   'top': (fieldCentrY - ball.outerHeight() / 2) + y,
-    //   'left': (fieldCentrX - ball.outerWidth() / 2) + x
-    // });
-
-    return {
-            y: (fieldCentrY - ball.outerHeight() / 2) + y,
-            x: (fieldCentrX - ball.outerWidth() / 2) + x
-           }
-  }
 });
+
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
